9bd92ef0ae12c247f7692ae9f0451f654a6e0227
Jatin Mehta <mehtajatin47.jm@gmail.com>
2020-06-07 23:59:44 +0530

    Add files via upload

---
+++ CodeFight/Chemistry_Homework.cpp
@@ -0,0 +1,40 @@
+std::string restoreChemicalFormula(std::vector<std::string> e, std::string f) {
+    unordered_map<string,int>m;
+    for(int i=0;i<e.size();i++)
+    m[e[i]]++;
+    vector<int>v(f.size()+1,0);
+    v[0]=-1;
+    string y="";
+    for(int i=0;i<f.size();i++)
+    f[i]=tolower(f[i]);
+    for(int i=0;i<=f.size();i++)
+    {
+        for(int j=1;j<=f.size()-i;j++)
+        {
+           if(v[i])
+           {
+              string k=f.substr(i,j);
+              k[0]=toupper(k[0]);
+              if(m.count(k) && j>v[j+i])
+              v[j+i]=j;
+           }
+        }
+
+       if(v[f.size()])
+        {
+            string u="";
+            int j=f.size();
+            cout<<v[j]<<" ";
+             while(j>0)
+            {
+                string q=f.substr(j-v[j],v[j]);
+                q[0]=toupper(q[0]);
+                u=q+u;
+                j-=v[j];
+            }
+            if(u<y || y=="")
+            y=u;
+        }
+    }
+    return y;
+}
